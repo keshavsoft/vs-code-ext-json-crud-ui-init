8963aad95294cc7da651380b2aa41675f62b8857
keshavsoft <nknnkumar@yahoo.com>
2026-05-26 18:49:57 +0530

    initHeaderFromCdn working

--- src/V2/ForEditorTitle/InitHeaderFromCdn/orchestration/runFeatureOrchestration.js
+++ src/V2/ForEditorTitle/InitHeaderFromCdn/orchestration/runFeatureOrchestration.js
@@ -2,30 +2,17 @@ import fs from 'fs';
 import path from 'path';
 import * as vscode from 'vscode';
 
-import StartEndPoint from "json-crud-ui-init";
-
-import { fileURLToPath } from 'url';
-import { copyTemplate } from '../services/copyTemplate.js';
+import { initHeaderFromCdn } from "json-crud-ui-init";
 
 export async function runFeatureOrchestration({ context }) {
     const endpoint = await getEndpoint();
     if (!endpoint) return null;
 
-    // fix inside localContext
-    const localContext = {
-        ...context,
-        endpointFolder: path.join(context.targetPath, endpoint),
-        routeFilePath: path.join(context.targetPath, endpoint),
-        templatePath: fileURLToPath(new URL('../templates/Base', import.meta.url))
-    };
-
-    const funcToRun = await StartEndPoint();
-
-    await funcToRun({
+    await initHeaderFromCdn({
         showLog: true,
         isAnnounce: true,
         folderName: endpoint,
-        toPath: localContext.targetPath
+        toPath: path.join(context.targetPath, endpoint)
     });
 
     return { endpoint };
@@ -40,9 +27,3 @@ async function getEndpoint() {
 
     return clean;
 };
-
-// change copy
-function copy({ templatePath, routeFilePath, endpointFolder }) {
-    if (!fs.existsSync(endpointFolder)) fs.mkdirSync(endpointFolder, { recursive: true });
-    copyTemplate({ templatePath, targetPath: routeFilePath });
-}
